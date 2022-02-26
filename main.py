6422f9cf3772d5d5b3fde36effbeef09d35bc378
YefersonGR <ygr-7@hotmail.com>
2022-02-25 20:56:53 -0500

    NTV001 Se crea ejemplo de 3 condicones

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #Comentario
 
+from cgi import print_form
+
+
 print("Que miedo de la guerra")
 
 nivelAgua=int(input("Digita la cantidad de agua de la represa: "))
@@ -7,3 +10,10 @@ nivelAgua=int(input("Digita la cantidad de agua de la represa: "))
 print(f"El nivel de agua es {nivelAgua}")
 
 print("Oe")
+
+if(nivelAgua<200):
+    print("No tengo agua")
+elif(nivelAgua>=200 and nivelAgua<450):
+    print("Todo bien energía corriente")
+else:
+    print("Ojo con caucasia")
